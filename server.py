49d08a4d1ff636264b23cc4c1d3806ee25fe9033
김재모 <rmramm7@naver.com>
2025-04-17 02:32:30 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from aiohttp import web
 import pathlib
-import os
-from diffusers import StableDiffusionPipeline
 import torch
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse,HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+import os
+from diffusers import DiffusionPipeline
 
 pipe = StableDiffusionPipeline.from_single_file(
     "C:/Users/AhnLab/Desktop/DreamShaper_8.safetensors",  # DreamShaper 파일 경로
@@ -18,17 +21,26 @@ generator = torch.manual_seed(33)
 
 
 
-routes = web.RouteTableDef()
+app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).parent
 IMAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")  # frontend 폴더 위치
+
+# frontend 폴더 mount
+app.mount("/frontend", StaticFiles(directory=FRONTEND_DIR), name="frontend")
+
+# static 파일들 mount
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+@app.get('/',response_class=HTMLResponse)
+async def serve_frontend():
+    with open(os.path.join(FRONTEND_DIR, "index.html"), encoding="utf-8") as f:
+        html = f.read()
+    return HTMLResponse(content=html)
 
 
-@routes.get('/')
-async def serve_html(request):
-    return web.FileResponse(BASE_DIR/"index.html")
-
-
-@routes.post("/submit")
+@app.post("/submit")
 async def handle_post(request):
     """클라이언트에서 JSON 데이터를 받아 응답하는 핸들러"""
     try:
@@ -36,10 +48,10 @@ async def handle_post(request):
 
         data = await request.json()
         text = data.get("text", "")
-        image = pipe(text, generator=generator, num_inference_steps=30).images[0]
+        #image = pipe(text, generator=generator, num_inference_steps=30).images[0]
 
         image_filename=text+'.png'
-        image.save(image_filename)
+        #image.save(image_filename)
         
         print(f"Received text: {image_filename}") 
 
@@ -48,26 +60,14 @@ async def handle_post(request):
         if not os.path.exists(image_path):
             image_path = os.path.join(IMAGE_DIR, "default.png")  # 기본 이미지 반환
         print(f"path:{image_path}")
-        return web.json_response({"status": "success", "image_url": f"/{image_filename}"})
+        return JSONResponse(content={"image_url": image_filename,"status":"success"})
         
 
     except Exception as e:
-        return web.json_response({"status": "error", "message": str(e)}, status=400)
+        return JSONResponse(content={"image_url": image_filename,"status":"success"})
     
-@routes.get("/{filename}")
-async def serve_image(request):
-    """이미지 제공 엔드포인트"""
-    filename = request.match_info["filename"]
-    image_path = os.path.join(IMAGE_DIR, filename)
-
-    if not os.path.exists(image_path):  # 파일이 없으면 기본 이미지 반환
-        image_path = os.path.join(IMAGE_DIR, "default.png")
-
-    return web.FileResponse(image_path)
 
 
-app = web.Application()
-app.add_routes(routes)
 
-if __name__=="__main__":
-    web.run_app(app, host='127.0.0.1', port=8080)
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="127.0.0.2", port=8000, reload=True)
